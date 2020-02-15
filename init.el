2a04773f3fa3c4e40cf520572f3a4b7cbacbb9a8
nteetor <nathanteetor@gmail.com>
2020-02-14 19:12:41 -0500

    unfill paragraph fun

--- init.el
+++ init.el
@@ -85,12 +85,21 @@ There are two things you can do about this warning:
   (concat (shorter-path (eshell/pwd))
           (if (= (user-uid) 0) " # " " $ ")))
 
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 ;;
 ;; GENERAL KEYBINDINGS
 ;;
 (global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "M-Q") 'unfill-paragraph)
 
 ;;
 ;; ESHELL PROMPT
